5051a3a981baa3cd9e89c4c1a9ea204d0c8ef44c
Bozca <abdullahbozca@gmail.com>
2022-03-01 23:56:40 +0300

    01/03/2022_02

--- src/test/java/get_http_request/GetRequest01.java
+++ src/test/java/get_http_request/GetRequest01.java
@@ -1,6 +1,7 @@
 package get_http_request;
 
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,6 +12,11 @@ public class GetRequest01 {
         String url="https://restful-booker.herokuapp.com/booking";
         Response response=given().when().get(url+"/2");//gidilen url deki bilgileri reponse değişkenine atar
         response.prettyPrint();//response variable ındaki dataları yazdırır.
+        System.out.println("Status Code : "+response.statusCode());
+        System.out.println("Content Type : "+response.contentType());
+        System.out.println("Test zamanı : "+response.time());
+
+        Assert.assertTrue(response.statusCode()==201);
 
     }
 }
